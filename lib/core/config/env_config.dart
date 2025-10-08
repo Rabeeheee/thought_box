49276d3d784a38db2dcfd439131abe1d3047a9ae
Rabeeh <rabeeh@example.com>
2025-10-08 18:16:16 +0530

    login initiated

--- lib/core/config/env_config.dart
+++ lib/core/config/env_config.dart
@@ -1,16 +1,14 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class EnvConfig {
-  static const String baseUrl = String.fromEnvironment(
-    'BASE_URL',
-    defaultValue: 'https://api.exconvert.com',
-  );
+  static String get baseUrl => dotenv.env['BASE_URL'] ?? 'https://api.exconvert.com';
   
-  static const String accessKey = String.fromEnvironment(
-    'ACCESS_KEY',
-    defaultValue: '270ca084-96a82de7-ae4aff0f-60b941d9',
-  );
+  static String get accessKey => dotenv.env['ACCESS_KEY'] ?? '270ca084-96a82de7-ae4aff0f-60b941d9';
   
-  static const bool useMockData = String.fromEnvironment(
-    'USE_MOCK',
-    defaultValue: 'false',
-  ) == 'true';
+  static bool get useMockData => dotenv.env['USE_MOCK'] == 'true';
+  
+  // Load environment
+  static Future<void> load({String fileName = '.env'}) async {
+    await dotenv.load(fileName: fileName);
+  }
 }
